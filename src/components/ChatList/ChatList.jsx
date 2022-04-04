a05a017f236d09e117c230170c13756b6e1de810
Elias <politico@mail.ru>
2022-04-04 15:37:56 +0300

    hw4 - react router

--- src/components/ChatList/ChatList.jsx
+++ src/components/ChatList/ChatList.jsx
@@ -1,6 +1,36 @@
+import { NavLink, Outlet } from "react-router-dom";
 import { Chat } from "../Chat/Chat"
 
-export const ChatList = ({ chatList }) => 
-    chatList.map((chat) => (
-        <Chat key={chat.id} name={chat.name} />
-    ));
+const chatList = [
+    {
+      id: "chat1",
+      name: "Richard Adams",
+    },
+    {
+      id: "chat2",
+      name: "Jenna Richards",
+    },
+    {
+      id: "chat3",
+      name: "Maria Antonova",
+    },
+    {
+      id: "chat4",
+      name: "Vasiliy Detochkin",
+    },
+];
+
+export const ChatList = () => (
+    <>
+        <div>
+            {chatList.map((chat) => (
+                <NavLink key={chat.id} to={`/chat/${chat.id}`}>
+                    <Chat name={chat.name} />
+                </NavLink>
+            ))}
+        </div>
+        <Outlet />
+    </>
+    
+)
+    
