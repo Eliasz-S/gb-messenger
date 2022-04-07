7428833040bc3a86a6bbf4cd36a0e322b9851004
Elias <politico@mail.ru>
2022-04-07 15:57:11 +0300

    hw5 - redux

--- src/screens/Profile/Profile.jsx
+++ src/screens/Profile/Profile.jsx
@@ -1,1 +1,23 @@
-export const Profile = () => <h4>This is your profile page</h4>;
+import { useDispatch, useSelector } from "react-redux";
+import { toggleCheckbox } from "../../store/profile/actions";
+
+export const Profile = () => {
+    const dispatch = useDispatch()
+    const state = useSelector(state => state);
+    console.log(state);
+    const handleCheckbox = () => {
+        dispatch(toggleCheckbox);
+    }
+    return (
+        <>
+            <h3>This is your profile page</h3>
+            <div style={ {height: 40} }>
+                {state.showName && <span>{state.name}</span>}
+            </div>
+            <div>
+                <input id="toggle_name" type="checkbox" onChange={handleCheckbox}/>
+                <label htmlFor="toggle_name">Show user name</label>
+            </div>
+        </>
+    );
+};
