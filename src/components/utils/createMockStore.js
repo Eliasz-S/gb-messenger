182c4685c235c89277c77a0768d5df7cb3128f76
Elias <politico@mail.ru>
2022-04-28 16:43:20 +0300

    more minor fixes

--- src/components/utils/createMockStore.js
+++ src/components/utils/createMockStore.js
@@ -1,9 +1,14 @@
-import configureMockStore from 'redux-mock-store'
-import thunk from "redux-thunk"
+import configureMockStore from 'redux-mock-store';
+import thunk from "redux-thunk";
 
 const middleware = [thunk];
+
 export const store = (initialState) => {
-  const mockStore = configureMockStore(middleware)(initialState)
-  mockStore.dispatch = jest.fn(mockStore.dispatch)
-  return mockStore
-}
+
+  const mockStore = configureMockStore(middleware)(initialState);
+
+  mockStore.dispatch = jest.fn(mockStore.dispatch);
+
+  return mockStore;
+
+};
