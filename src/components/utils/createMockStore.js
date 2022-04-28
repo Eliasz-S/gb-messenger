2a556cffa392b18e17f23e22053ac8ae123fd2be
Elias <politico@mail.ru>
2022-04-28 16:41:38 +0300

    minor fixes

--- src/components/utils/createMockStore.js
+++ src/components/utils/createMockStore.js
@@ -4,8 +4,6 @@ import thunk from "redux-thunk"
 const middleware = [thunk];
 export const store = (initialState) => {
   const mockStore = configureMockStore(middleware)(initialState)
-  // eslint-disable-next-line no-undef
-  // замените метод dispatch "шпионом" и поддерживайте  функциональность
   mockStore.dispatch = jest.fn(mockStore.dispatch)
   return mockStore
 }
